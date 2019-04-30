450eaa2dca11b97440f264c41af1aeacce167d95
Catriena Wood <Catriena.wood@pcc.edu>
2019-04-29 19:55:52 -0700

    print date

--- src/PCC/CIS/Hello_World/HelloWorld.java
+++ src/PCC/CIS/Hello_World/HelloWorld.java
@@ -1,7 +1,10 @@
 package PCC.CIS.Hello_World;
 
+import java.util.Date;
+
 public class HelloWorld {
     public static void main (String [] args){
         System.out.println("Hi there!");
+        System.out.println(new Date());
     }
 }
